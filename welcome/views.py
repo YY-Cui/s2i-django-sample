3902354a203afc878a340a55d2fa1aa4af106ee5
You Yi TU Cui <cuiyouyi@cn.ibm.com>
2021-11-30 22:24:29 +0800

    修改了一些代码

--- welcome/views.py
+++ welcome/views.py
@@ -9,15 +9,7 @@ from .models import PageView
 # Create your views here.
 
 def index(request):
-    """Takes an request object as a parameter and creates an pageview object then responds by rendering the index view."""
-    hostname = os.getenv('HOSTNAME', 'unknown')
-    PageView.objects.create(hostname=hostname)
-
-    return render(request, 'welcome/index.html', {
-        'hostname': hostname,
-        'database': database.info(),
-        'count': PageView.objects.count()
-    })
+    return render(request, 'welcome/index.html')
 
 def health(request):
     """Takes an request as a parameter and gives the count of pageview objects as reponse"""

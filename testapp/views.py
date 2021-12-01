b69e88225b6c466f2f3192b9fe2f619838097ee8
You Yi TU Cui <cuiyouyi@cn.ibm.com>
2021-12-01 11:32:40 +0800

    添加应用

--- testapp/views.py
+++ testapp/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def index(request):
     data = [[1], [1,1]]
     for i in range(3, 10):
@@ -11,9 +12,10 @@ def index(request):
             l.append(lastline[j] + lastline[j+1])
         l.append(1)
         data.append(l)
+    # 生成字符串
     sep = '&nbsp;&nbsp;&nbsp;&nbsp;'
     width = len(sep.join(map(str, data[-1])))
     res_str = '<br/><br/>'
     for i in data:
         res_str += '<p style="text-align:center">' + sep.join(map(str, i)) + '</p>'
-    return HttpResponse(res_str + '<br/><h5 style="text-align:center">一个9层的杨辉三角</h5>')
+    return HttpResponse(res_str + '<br/><h5 style="text-align:center">9层的杨辉三角</h5>')
